80b799a2ece4843a7133b027ee72f2bb0619cfea
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-01 12:12:20 +0530

    add twitter provder

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import nextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google"
+import TwitterProvider from "next-auth/providers/twitter"
 
 export const authOptions = {
   secret:process.env.NEXTAUTH_SECRET,
@@ -13,6 +14,10 @@ export const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
     }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_CLIENT_ID as string,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET as string
+    })
   ],
 };
 
