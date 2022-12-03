109b9e81d127c0dd5b09b9ed446db639e641a51a
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-03 22:34:41 +0530

    Add video to the custom signin page

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -2,14 +2,27 @@ import { getProviders, signIn, getSession } from "next-auth/react";
 
 const SignIn = ({ providers }: any) => {
   return (
-    <div>
-      {Object.values(providers).map((provider: any) => (
-        <div key={provider.name}>
-          <button onClick={() => signIn(provider.id)}>
-            Sign in with {provider.name}
-          </button>
-        </div>
-      ))}
+    <div className="relative">
+      <div>
+        <video
+          className="w-full object-cover h-screen"
+          autoPlay
+          muted
+          loop
+          src="/assets/background-img.mp4"
+        >
+          <source src="/assets/background-img.mp4" type="video/mp4" />
+        </video>
+      </div>
+      <div className="absolute top-3">
+        {Object.values(providers).map((provider: any) => (
+          <div key={provider.name}>
+            <button onClick={() => signIn(provider.id)}>
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
