b71b99cf455017c6eb4f46ac58643fcbc5425797
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-05 21:29:18 +0530

    build fix

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -15,16 +15,17 @@ const SignIn = ({ providers }: any) => {
         </video>
       </div>
       <div className="absolute top-1/3 left-12 sm:left-40 md:left-1/3 mx-3 md:mx-6 bg-white bg-opacity-20 backdrop-blur-md px-5 md:px-10 py-7 rounded-lg drop-shadow-2xl">
-        {Object.values(providers).map((provider: any) => (
-          <div key={provider.name} className="my-4">
-            <button
-              onClick={() => signIn(provider.id)}
-              className="bg-[#0c4a6e] transition delay-100 hover:bg-[#fb7185] w-full py-4 px-3 rounded-md text-white"
-            >
-              Sign in with {provider.name}
-            </button>
-          </div>
-        ))}
+        {providers &&
+          Object.values(providers).map((provider: any) => (
+            <div key={provider?.name} className="my-4">
+              <button
+                onClick={() => signIn(provider?.id)}
+                className="bg-[#0c4a6e] transition delay-100 hover:bg-[#fb7185] w-full py-4 px-3 rounded-md text-white"
+              >
+                Sign in with {provider?.name}
+              </button>
+            </div>
+          ))}
       </div>
     </div>
   );
