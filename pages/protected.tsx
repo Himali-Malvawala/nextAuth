703e1b2250a8d807550036dd88a8fea2da6c4eed
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-05 12:57:46 +0530

    fix

--- pages/protected.tsx
+++ pages/protected.tsx
@@ -1,16 +1,16 @@
 import { useSession } from "next-auth/react";
 
-const Protected = () => {
-  const { data: session } = useSession();
-  console.log("session:", session);
+// const Protected = () => {
+//   const { data: session } = useSession();
+//   console.log("session:", session);
 
-  if (session === undefined) {
-    return {
-      redirect: { destination: "/" },
-    };
-  }
+//   if (session === undefined) {
+//     return {
+//       redirect: { destination: "/" },
+//     };
+//   }
 
-  return <div>okokokok</div>;
-};
+//   return <div>okokokok</div>;
+// };
 
-export default Protected;
+// export default Protected;
