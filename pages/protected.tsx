4c6a1429b50dae61bb181ccaeacb0a12866ef476
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-05 15:06:30 +0530

    build error

--- pages/protected.tsx
+++ pages/protected.tsx
@@ -1,16 +1,14 @@
 import { useSession } from "next-auth/react";
 
-// const Protected = () => {
-//   const { data: session } = useSession();
-//   console.log("session:", session);
+const Protected = () => {
+  //   const { data: session } = useSession();
+  //   console.log("session:", session);
 
-//   if (session === undefined) {
-//     return {
-//       redirect: { destination: "/" },
-//     };
-//   }
-
-//   return <div>okokokok</div>;
-// };
-
-// export default Protected;
+  //   if (session === undefined) {
+  //     return {
+  //       redirect: { destination: "/" },
+  //     };
+  //   }
+  return <div>okokokok</div>;
+};
+export default Protected;
