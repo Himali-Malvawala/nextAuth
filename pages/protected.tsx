10505c0241da33e77ba239bc46016e574a0fcecc
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-06 17:05:27 +0530

    add video to protected page

--- pages/protected.tsx
+++ pages/protected.tsx
@@ -4,18 +4,31 @@ const Protected = () => {
   const { data: session } = useSession();
   console.log("session: ", session);
   return (
-    <div>
-      <h1>Access Granted</h1>
-      <p>This is the protected Content!! (lol)</p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+    <div className="relative">
+      <div>
+        <video
+          className="w-full object-cover h-screen"
+          autoPlay
+          muted
+          loop
+          src="/assets/background-img.mp4"
+        >
+          <source src="/assets/background-img.mp4" type="video/mp4" />
+        </video>
+      </div>
+      <div>
+        <h1>Access Granted</h1>
+        <p>This is the protected Content!! (lol)</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
     </div>
   );
 };
