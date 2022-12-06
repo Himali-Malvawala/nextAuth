c30f34fae7cf9d311164d7a22803a1eca31f2b12
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-06 17:32:44 +0530

    Add status

--- pages/protected.tsx
+++ pages/protected.tsx
@@ -1,9 +1,9 @@
-import { Link } from "@mui/material";
-import { useSession } from "next-auth/react";
+import { Link, Typography } from "@mui/material";
+import { signIn, useSession } from "next-auth/react";
 
 const Protected = () => {
-  const { data: session } = useSession();
-  console.log("session: ", session);
+  const { data: session, status } = useSession();
+
   return (
     <div className="relative">
       <div>
@@ -25,23 +25,41 @@ const Protected = () => {
             </p>
           </Link>
         </div>
-        <div className="absolute top-24 mx-3 md:mx-6 bg-white bg-opacity-20 backdrop-blur-md px-5 md:px-10 py-7 rounded-lg drop-shadow-2xl sm:w-11/12 flex flex-col justify-center items-center text-center text-white">
-          <h1 className="text-xl md:text-2xl font-bold text-green-500">
-            Access Granted
-          </h1>
-          <p className="text-lg md:text-xl font-medium mt-3">
-            This is the protected Content (lol)
-          </p>
-          <p className="text-lg mt-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </div>
+
+        {!session && status === "unauthenticated" && (
+          <div className="absolute top-24 mx-3 md:mx-6 bg-white bg-opacity-20 backdrop-blur-md px-5 md:px-10 py-7 rounded-lg drop-shadow-2xl sm:w-11/12 flex flex-col justify-center items-center text-center text-white">
+            <h1 className="text-xl md:text-2xl font-bold text-red-500">
+              Access Denied
+            </h1>
+            <Link
+              underline="none"
+              onClick={() => signIn(undefined, { callbackUrl: "/" })}
+            >
+              <Typography className="text-lg font-medium mt-3 underline underline-offset-[3px] cursor-pointer text-white">
+                You must be signed in to view this page
+              </Typography>
+            </Link>
+          </div>
+        )}
+        {session && status === "authenticated" && (
+          <div className="absolute top-24 mx-3 md:mx-6 bg-white bg-opacity-20 backdrop-blur-md px-5 md:px-10 py-7 rounded-lg drop-shadow-2xl sm:w-11/12 flex flex-col justify-center items-center text-center text-white">
+            <h1 className="text-xl md:text-2xl font-bold text-green-500">
+              Access Granted
+            </h1>
+            <p className="text-lg md:text-xl font-medium mt-3">
+              This is the protected Content (lol)
+            </p>
+            <p className="text-lg mt-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
