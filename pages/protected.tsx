f7d0ebb8bdf44c07297876a5e40f619310e3439a
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-06 17:18:50 +0530

    Style the text

--- pages/protected.tsx
+++ pages/protected.tsx
@@ -25,17 +25,23 @@ const Protected = () => {
             </p>
           </Link>
         </div>
-        <h1>Access Granted</h1>
-        <p>This is the protected Content!! (lol)</p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <div className="absolute top-24 mx-3 md:mx-6 bg-white bg-opacity-20 backdrop-blur-md px-5 md:px-10 py-7 rounded-lg drop-shadow-2xl sm:w-11/12 flex flex-col justify-center items-center text-center text-white">
+          <h1 className="text-xl md:text-2xl font-bold text-green-500">
+            Access Granted
+          </h1>
+          <p className="text-lg md:text-xl font-medium mt-3">
+            This is the protected Content (lol)
+          </p>
+          <p className="text-lg mt-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
       </div>
     </div>
   );
