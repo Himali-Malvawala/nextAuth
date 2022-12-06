b935425c008c4fe3676a6c0d11926cc0f7b0c5a9
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-06 17:04:07 +0530

    add protected content

--- pages/protected.tsx
+++ pages/protected.tsx
@@ -1,14 +1,22 @@
 import { useSession } from "next-auth/react";
 
 const Protected = () => {
-  //   const { data: session } = useSession();
-  //   console.log("session:", session);
-
-  //   if (session === undefined) {
-  //     return {
-  //       redirect: { destination: "/" },
-  //     };
-  //   }
-  return <div>okokokok</div>;
+  const { data: session } = useSession();
+  console.log("session: ", session);
+  return (
+    <div>
+      <h1>Access Granted</h1>
+      <p>This is the protected Content!! (lol)</p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </div>
+  );
 };
 export default Protected;
