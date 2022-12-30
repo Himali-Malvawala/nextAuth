4262b3ddf08ea38eb68b1eb3419d88aa3fd31a18
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-30 17:00:56 +0530

    Add email provider and mongo clinet

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,6 +3,8 @@ import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google"
 import TwitterProvider from "next-auth/providers/twitter"
 import EmailProvider from "next-auth/providers/email"
+import {MongoDBAdapter} from "@next-auth/mongodb-adapter"
+import clientPromise from "../../../lib/mongodb";
 
 export const authOptions = {
   secret:process.env.NEXTAUTH_SECRET,
@@ -19,18 +21,20 @@ export const authOptions = {
       clientId: process.env.TWITTER_CLIENT_ID as string,
       clientSecret: process.env.TWITTER_CLIENT_SECRET as string
     }),
-    // EmailProvider({
-    //   server: {
-    //     host: process.env.EMAIL_SERVER_HOST,
-    //     port: process.env.EMAIL_SERVER_PORT,
-    //     auth: {
-    //       user: process.env.EMAIL_SERVER_USER,
-    //       pass: process.env.EMAIL_SERVER_PASSWORD
-    //     }
-    //   },
-    //   from: process.env.EMAIL_FROM
-    // }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD
+        }
+      },
+      from: process.env.EMAIL_FROM,
+      maxAge: 10 * 60, // Magic links are valid for 10 min only
+    }),
    ],
+   adapter: MongoDBAdapter(clientPromise),
    pages: {
     signIn: '/auth/signin'
    }
